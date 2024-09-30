5dd89eedcb924b76148f398f86182628ed518a5d
Joaquín Zúñiga <zunigacastrojf@gmail.com>
2024-09-30 18:24:42 -0300

    archivo modificado

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // La carpeta donde se generará el build
-    rollupOptions: {
-      input: './index.html',  // Asegúrate de que apunte a tu archivo `index.html`
-    },
-  },
-  base: '/PizzeriaMamaMia/'  // Asegúrate de que la base apunte al subdirectorio de GitHub Pages
+  base: '/PizzeriaMamaMia/' // Ruta base para GitHub Pages
 });
