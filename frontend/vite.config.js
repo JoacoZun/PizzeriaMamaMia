59b011c29eff262e5da8228b2862cfeef3a5d714
Joaquín Zúñiga <zunigacastrojf@gmail.com>
2024-09-30 18:34:02 -0300

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PizzeriaMamaMia/' // Ruta base para GitHub Pages
+  base: '/PizzeriaMamaMia/', 
 });
