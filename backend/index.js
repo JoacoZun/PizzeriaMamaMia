9d537306e0e401a8dfe57ea6606fdc68db85c963
Joaquín Zúñiga <zunigacastrojf@gmail.com>
2024-10-12 21:12:52 -0300

    creacion archivos

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,18 @@
-import cors from "cors";
-import "dotenv/config";
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-import authRoute from "./routes/auth.route.js";
-import checkoutRoute from "./routes/checkout.route.js";
-import pizzaRoute from "./routes/pizza.route.js";
+dotenv.config();
 
 const app = express();
-
+app.use(cors()); // Habilita CORS
 app.use(express.json());
-app.use(cors());
 
-app.use("/api/auth", authRoute);
-app.use("/api/pizzas", pizzaRoute);
-app.use("/api/checkouts", checkoutRoute);
-app.use((_, res) => {
-  res.status(404).json({ error: "Not Found" });
+app.get('/api/pizzas', (req, res) => {
+  res.json([{ id: 1, name: 'Margarita' }]);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Backend running on port ${PORT}`);
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PizzeriaMamaMia/', 
+  base: '/PizzeriaMamaMia/', // Ruta relativa para GitHub Pages
 });
