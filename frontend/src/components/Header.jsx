44bd6598bf26e20cd74978c44165f133701b79c9
JoacoZun <zunigacastrojf@gmail.com>
2024-10-17 20:37:20 -0300

    achivo editado

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>¡Pizzería Mamma Mia!</h1>
+      <h1>¡Pizzería Mamma Mia! 2</h1>
       <hr></hr>
       <p>¡Tenemos las mejores pizzas que podrás encontrar!</p>
     </header>
